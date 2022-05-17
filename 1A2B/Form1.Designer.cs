51ce8846cf0f671caf51305054ff4b8078943fe8
yang <yang87924@gmail.com>
2022-05-17 23:41:54 +0800

    first commit

---
+++ 1A2B/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace _1A2B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.answer_buttom = new System.Windows.Forms.Button();
+            this.input_value = new System.Windows.Forms.TextBox();
+            this.show = new System.Windows.Forms.Label();
+            this.guess_button = new System.Windows.Forms.Button();
+            this.angin_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Guess_richTextBox = new System.Windows.Forms.RichTextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.難度ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.初級ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.中級ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終極版未完成ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // answer_buttom
+            // 
+            this.answer_buttom.Location = new System.Drawing.Point(781, 429);
+            this.answer_buttom.Margin = new System.Windows.Forms.Padding(4);
+            this.answer_buttom.Name = "answer_buttom";
+            this.answer_buttom.Size = new System.Drawing.Size(100, 29);
+            this.answer_buttom.TabIndex = 0;
+            this.answer_buttom.Text = "看答案";
+            this.answer_buttom.UseVisualStyleBackColor = true;
+            this.answer_buttom.Click += new System.EventHandler(this.answer_Click);
+            // 
+            // input_value
+            // 
+            this.input_value.Location = new System.Drawing.Point(345, 143);
+            this.input_value.Margin = new System.Windows.Forms.Padding(4);
+            this.input_value.Name = "input_value";
+            this.input_value.Size = new System.Drawing.Size(132, 25);
+            this.input_value.TabIndex = 2;
+            this.input_value.TextChanged += new System.EventHandler(this.input_value_TextChanged);
+            this.input_value.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.input_value_KeyPress);
+            // 
+            // show
+            // 
+            this.show.AutoSize = true;
+            this.show.Font = new System.Drawing.Font("微軟正黑體", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.show.Location = new System.Drawing.Point(703, 107);
+            this.show.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(0, 48);
+            this.show.TabIndex = 3;
+            // 
+            // guess_button
+            // 
+            this.guess_button.Enabled = false;
+            this.guess_button.Location = new System.Drawing.Point(781, 218);
+            this.guess_button.Margin = new System.Windows.Forms.Padding(4);
+            this.guess_button.Name = "guess_button";
+            this.guess_button.Size = new System.Drawing.Size(100, 29);
+            this.guess_button.TabIndex = 4;
+            this.guess_button.Text = "確定";
+            this.guess_button.UseVisualStyleBackColor = true;
+            this.guess_button.Click += new System.EventHandler(this.guess_button_Click);
+            // 
+            // angin_button
+            // 
+            this.angin_button.Location = new System.Drawing.Point(781, 311);
+            this.angin_button.Margin = new System.Windows.Forms.Padding(4);
+            this.angin_button.Name = "angin_button";
+            this.angin_button.Size = new System.Drawing.Size(100, 29);
+            this.angin_button.TabIndex = 5;
+            this.angin_button.Text = "重玩";
+            this.angin_button.UseVisualStyleBackColor = true;
+            this.angin_button.Click += new System.EventHandler(this.angin_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(340, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 30);
+            this.label1.TabIndex = 6;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Guess_richTextBox
+            // 
+            this.Guess_richTextBox.Location = new System.Drawing.Point(217, 201);
+            this.Guess_richTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Guess_richTextBox.Name = "Guess_richTextBox";
+            this.Guess_richTextBox.ReadOnly = true;
+            this.Guess_richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.Guess_richTextBox.Size = new System.Drawing.Size(369, 407);
+            this.Guess_richTextBox.TabIndex = 8;
+            this.Guess_richTextBox.Text = "";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.難度ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(941, 27);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 難度ToolStripMenuItem
+            // 
+            this.難度ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.初級ToolStripMenuItem,
+            this.中級ToolStripMenuItem,
+            this.終極版未完成ToolStripMenuItem});
+            this.難度ToolStripMenuItem.Name = "難度ToolStripMenuItem";
+            this.難度ToolStripMenuItem.Size = new System.Drawing.Size(51, 23);
+            this.難度ToolStripMenuItem.Text = "難度";
+            // 
+            // 初級ToolStripMenuItem
+            // 
+            this.初級ToolStripMenuItem.Name = "初級ToolStripMenuItem";
+            this.初級ToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.初級ToolStripMenuItem.Text = "初級";
+            this.初級ToolStripMenuItem.Click += new System.EventHandler(this.初級ToolStripMenuItem_Click);
+            // 
+            // 中級ToolStripMenuItem
+            // 
+            this.中級ToolStripMenuItem.Name = "中級ToolStripMenuItem";
+            this.中級ToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.中級ToolStripMenuItem.Text = "中級";
+            this.中級ToolStripMenuItem.Click += new System.EventHandler(this.中級ToolStripMenuItem_Click);
+            // 
+            // 終極版未完成ToolStripMenuItem
+            // 
+            this.終極版未完成ToolStripMenuItem.Name = "終極版未完成ToolStripMenuItem";
+            this.終極版未完成ToolStripMenuItem.Size = new System.Drawing.Size(184, 26);
+            this.終極版未完成ToolStripMenuItem.Text = "終極版(未完成)";
+            this.終極版未完成ToolStripMenuItem.Click += new System.EventHandler(this.終極版未完成ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 629);
+            this.Controls.Add(this.Guess_richTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.angin_button);
+            this.Controls.Add(this.guess_button);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.input_value);
+            this.Controls.Add(this.answer_buttom);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "1A2B(第十組專題報告)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button answer_buttom;
+        private System.Windows.Forms.TextBox input_value;
+        private System.Windows.Forms.Label show;
+        private System.Windows.Forms.Button guess_button;
+        private System.Windows.Forms.Button angin_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox Guess_richTextBox;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 難度ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 初級ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 中級ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終極版未完成ToolStripMenuItem;
+    }
+}
+
---
+++ 1A2B/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1A2B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] number = new string[5];//存放答案的陣列
+        
+        string[] player_number=new string[5];//用來存放玩家輸入的值
+        Random rand = new Random();//產生亂數
+        int a=0, b=0;//用來存放幾A幾B的值
+        int totle = 0;//用來存放猜幾次
+        int several=4;//用來存放幾位數的值
+        int width;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Location = new Point(200, 28);
+            width = this.Width;
+            Array.Resize(ref player_number, 4);
+            //Array.Clear(player_number, 0, player_number.Length);
+            main();//當視窗載入時產生一次亂數
+            show.Visible = false;//一開始先讓答案不顯示
+            label1.Text = "請輸入" + several + "個不重複的0~9數字";//視窗載入時顯示遊戲說明
+        }
+        
+        private void 初級ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(200, 28);
+            Array.Resize(ref player_number, 4);
+            several = 4;//當初級被點選時先把位數設成4，代表現在是在玩猜幾位數的
+            input_value.ReadOnly = false;
+            angin_button.Enabled = true;
+            totle = 0;//把累計的猜測次數歸零
+            show.Text = "";//清空答案
+            show.Visible = false;//讓答案隱藏起來
+            answer_buttom.Enabled = true;//讓顯示答案的按鈕可以按
+            main();//產生一次亂數
+            input_value.Text = "";//清空輸入欄位
+            Guess_richTextBox.Text = "";//清空遊戲紀錄
+            label1.Text = "請輸入" + several + "個不重複的0~9數字";//當位數改變時重新更新一次遊戲說明
+        }
+        private void 中級ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(200, 28);
+            Array.Resize(ref player_number, 5);
+            several = 5;//當初級被點選時先把位數設成5，代表現在是在玩猜幾位數的
+            input_value.ReadOnly = false;
+            angin_button.Enabled = true;
+            totle = 0;//把累計的猜測次數歸零
+            show.Text = "";//清空答案
+            show.Visible = false;//讓答案隱藏起來
+            answer_buttom.Enabled = true;//讓顯示答案的按鈕可以按
+            main();//產生一次亂數
+            input_value.Text = "";//清空輸入欄位
+            Guess_richTextBox.Text = "";//清空遊戲紀錄
+            label1.Text = "請輸入" + several + "個不重複的0~9數字";//當位數改變時重新更新一次遊戲說明
+            //main();
+        }
+        private void main ()//產生一組四位不重複的數值
+        {
+            int i,j;
+            for(i=1;i <= several; i++)
+            {
+                do
+                {
+                    number[i - 1] =Convert.ToString( rand.Next(0, 9));
+
+                    for (j = 1; j < i; j++)
+                    {
+                        if (number[i - 1] == number[j - 1])
+                        {
+                            break;
+                        }
+                    }
+                    
+                } while (j != i);
+                show.Text += number[i - 1];
+            }
+        }
+        private void answer_Click(object sender, EventArgs e)//看答案
+        {
+            show.Visible = true;
+            answer_buttom.Enabled = false;
+        }
+
+        private void guess_button_Click(object sender, EventArgs e)//當猜測按鈕被按下時
+        {
+            Cutting_input_value();
+            for(int k=0;k < several; k++)
+            {
+                
+                for(int j=0;j < several; j++)
+                {
+                    if(number[k]==player_number[j]&& (number[k] != player_number[k]))
+                    {
+                        b++;
+                    }
+                }
+                if (number[k] == player_number[k])
+                {
+                    a++;
+                }
+            }
+            if(a == several)
+            {
+                timer2.Start();
+            }
+            Guess_richTextBox.Text +=input_value.Text+Environment.NewLine +a + "A" + b + "B"+ Environment.NewLine+ Environment.NewLine;
+            Guess_richTextBox.Select(Guess_richTextBox.Text.Length, 1);//用來讓紀錄欄位定位在最後面
+            Guess_richTextBox.ScrollToCaret();//用來讓紀錄欄位定位在最後面
+            a = 0;
+            b = 0;
+            totle++;
+        }
+
+        public void Cutting_input_value()//玩家輸入的字串分割存入陣列裡面
+        {
+            string a = Convert.ToString(input_value.Text);
+            
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                player_number[i] = a.Substring(i, 1);
+            }
+        }
+        private void input_value_TextChanged(object sender, EventArgs e)//當輸入欄位改變時做哪些事
+        {
+            int[] zxc = new int[10];
+            int sum = 0;
+           
+            string a = Convert.ToString(input_value.Text);
+
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                player_number[i] = a.Substring(i, 1);
+            }
+
+            foreach (string q in player_number)
+            {
+
+                zxc[Convert.ToInt32(q)] = 1;
+            }
+            
+            foreach (int q in zxc)
+            {
+                if (q == 1)
+                    sum++;
+               /* if(several=4&&sum<4)
+                {
+
+                }*/
+               
+                Console.WriteLine(sum);
+                Console.Read();
+            }
+            if (sum == several && input_value.Text.Length == several && isNumeric(input_value.Text))
+                guess_button.Enabled = true;
+            else
+                guess_button.Enabled = false;
+            
+        }
+        private void angin_button_Click(object sender, EventArgs e)//當重來按鈕被點擊
+        {
+            totle = 0;
+            show.Text = "";
+            show.Visible = false;
+            answer_buttom.Enabled = true;
+            main();
+            input_value.Text = "";
+            Guess_richTextBox.Text = "";
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            MessageBox.Show("雖然你猜對了，但你用了"+totle+"次","遊戲結束");
+            totle = 0;
+            show.Text = "";
+            show.Visible = false;
+            answer_buttom.Enabled = true;
+            main();
+            input_value.Text = "";
+            Guess_richTextBox.Text = "";
+        }
+        private void input_value_KeyPress(object sender, KeyPressEventArgs e)//用來判斷哪個按鍵被按下
+        {
+            if ((e.KeyChar < 48 | e.KeyChar > 57) & e.KeyChar != 8)
+                e.Handled = true;
+            if (input_value.Text.Length == several)
+                e.Handled = true;           
+            if (e.KeyChar == 8)
+                e.Handled = false;
+        }
+        public bool isNumeric(string str)
+        {
+            int i; char ch;
+            for (i = 0; i < str.Length; i++)
+            {
+                ch = str[i];
+                if (ch < '0' || ch > '9')
+                    return false;
+            }
+            return true;
+        }
+
+        private void 終極版未完成ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Location = new Point(150,28);
+            label1.Text = "從26個英文大寫字母中\n隨機挑選5個不重複的英文大寫字母\n來進行排列組合藉此來進行1A2B";
+            input_value.ReadOnly= true;
+            angin_button.Enabled = false;
+            answer_buttom.Enabled = false;
+            show.Text = "";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*int[] zxc = new int[10];
+            int sum = 0;
+
+            string a = Convert.ToString(input_value.Text);
+
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                player_number[i] = a.Substring(i, 1);
+            }
+
+            foreach (string q in player_number)
+            {
+                zxc[Convert.ToInt32(q)] = 1;
+            }
+            foreach (int q in zxc)
+            {
+                if (q == 1)
+                    sum++;
+            }
+            if (sum == 5 && input_value.Text.Length == several && isNumeric(input_value.Text))
+                guess_button.Enabled = true;
+            else
+                guess_button.Enabled = false;*/
+        }
+
+    }
+}
